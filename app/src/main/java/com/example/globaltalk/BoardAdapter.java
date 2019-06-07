71f36a022b02bd423a387da1486d515e0fd8bd40
wpdusdlrj97 <wpdusdlrj97@gmail.com>
2019-06-07 14:14:55 +0900

    board_upload

--- app/src/main/java/com/example/globaltalk/BoardAdapter.java
+++ app/src/main/java/com/example/globaltalk/BoardAdapter.java
@@ -3,6 +3,7 @@ package com.example.globaltalk;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -81,137 +82,162 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
     public void onBindViewHolder(@NonNull CustomViewHolder viewholder, int position) {
 
 
+        Log.d("보드 포지션", String.valueOf(position));
 
         viewholder.board_list_name.setText(bList.get(position).getwriter());
+        Log.d("보드 네임", bList.get(position).getwriter());
+
         viewholder.board_list_time.setText(bList.get(position).getwdate());
+        Log.d("보드 날짜", bList.get(position).getwdate());
+
         viewholder.board_list_text.setText(bList.get(position).getcontent());
+        Log.d("보드 내용", bList.get(position).getcontent());
 
 
         //0
         if(bList.get(position).getimg0().equals("null")){
             viewholder.board_list_image0.setVisibility(View.GONE);
+            Log.d("g 보드 이미지0", bList.get(position).getimg0());
         }else{
+            viewholder.board_list_image0.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg0())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image0);
+
+            Log.d("v 보드 이미지0", bList.get(position).getimg0());
         }
 
         //1
         if(bList.get(position).getimg1().equals("null")){
             viewholder.board_list_image1.setVisibility(View.GONE);
+            Log.d("g 보드 이미지1", bList.get(position).getimg1());
         }else{
+            viewholder.board_list_image1.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg1())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image1);
+            Log.d("v 보드 이미지1", bList.get(position).getimg1());
         }
 
         //2
         if(bList.get(position).getimg2().equals("null")){
             viewholder.board_list_image2.setVisibility(View.GONE);
+            Log.d("g 보드 이미지2", bList.get(position).getimg2());
         }else{
+            viewholder.board_list_image2.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg2())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image2);
+            Log.d("v 보드 이미지2", bList.get(position).getimg2());
         }
 
         //3
         if(bList.get(position).getimg3().equals("null")){
             viewholder.board_list_image3.setVisibility(View.GONE);
+            Log.d("g 보드 이미지3", bList.get(position).getimg3());
         }else{
+            viewholder.board_list_image3.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg3())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image3);
+            Log.d("v 보드 이미지3", bList.get(position).getimg3());
         }
 
         // 4
         if(bList.get(position).getimg4().equals("null")){
             viewholder.board_list_image4.setVisibility(View.GONE);
+            Log.d("g 보드 이미지4", bList.get(position).getimg4());
         }else{
+            viewholder.board_list_image4.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg4())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image4);
+            Log.d("v 보드 이미지4", bList.get(position).getimg4());
         }
 
         //5
         if(bList.get(position).getimg5().equals("null")){
             viewholder.board_list_image5.setVisibility(View.GONE);
+            Log.d("g 보드 이미지5", bList.get(position).getimg5());
         }else{
+            viewholder.board_list_image5.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg5())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image5);
+            Log.d("v 보드 이미지5", bList.get(position).getimg5());
         }
 
         //6
         if(bList.get(position).getimg6().equals("null")){
             viewholder.board_list_image6.setVisibility(View.GONE);
+            Log.d("g 보드 이미지6", bList.get(position).getimg6());
         }else{
+            viewholder.board_list_image6.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg6())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image6);
+            Log.d("v 보드 이미지6", bList.get(position).getimg6());
         }
 
 
         //7
         if(bList.get(position).getimg7().equals("null")){
             viewholder.board_list_image7.setVisibility(View.GONE);
+            Log.d("g 보드 이미지7", bList.get(position).getimg7());
         }else{
+            viewholder.board_list_image7.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg7())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image7);
+            Log.d("v 보드 이미지7", bList.get(position).getimg7());
         }
 
         //8
         if(bList.get(position).getimg8().equals("null")){
             viewholder.board_list_image8.setVisibility(View.GONE);
+            Log.d("g 보드 이미지8", bList.get(position).getimg8());
         }else{
+            viewholder.board_list_image8.setVisibility(View.VISIBLE);
             Glide.with(context)
                     .load(bList.get(position).getimg8())
-                    .placeholder(R.mipmap.ic_launcher)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .skipMemoryCache(true)
                     .thumbnail(0.1f)
                     .fitCenter()
                     .into(viewholder.board_list_image8);
+            Log.d("v 보드 이미지8", bList.get(position).getimg8());
         }
 
 
