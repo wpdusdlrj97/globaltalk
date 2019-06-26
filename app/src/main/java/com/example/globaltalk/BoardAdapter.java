62209648d352df08c6ee5699b8603ee32504a08c
wpdusdlrj97 <wpdusdlrj97@gmail.com>
2019-06-26 20:34:44 +0900

    edit

--- app/src/main/java/com/example/globaltalk/BoardAdapter.java
+++ app/src/main/java/com/example/globaltalk/BoardAdapter.java
@@ -7,7 +7,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -25,9 +27,16 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -42,11 +51,14 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
     private ArrayList<BoardData> bList = null;
     private Activity context = null;
 
-    String clientId = "cv81DQOKSlPw4lX1hyEe";//애플리케이션 클라이언트 아이디값";
-    String clientSecret = "s3Rf0jQDf0";//애플리케이션 클라이언트 시크릿값";
+    String clientId = "";//애플리케이션 클라이언트 아이디값";
+    String clientSecret = "";//애플리케이션 클라이언트 시크릿값";
 
-    String sourceLang="ko";
-    String targetLang="en";
+    String sourceHolder;
+    String targetHolder;
+
+    String sourceLang;
+    String targetLang;
 
 
     ProgressDialog progressDialog;
@@ -58,7 +70,12 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
     int heart=0;
 
 
-    String TextHolder;
+    private NaverDetectionTask mNaverDetectionTask;
+    private NaverTTSTask mNaverTTSTask;
+
+    //TTS 대상
+    String[] mTextString;
+    static String speaker;
 
 
 
@@ -106,13 +123,14 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
         protected ImageView board_list_image8;
 
 
-        protected TextView board_target_translate;
 
         protected ImageView board_write_heart;
 
         protected ImageView board_write_translate;
 
         protected ImageView board_write_more;
+
+        protected ImageView board_write_sound;
 
 
 
@@ -146,8 +164,9 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
 
             //번역
             this.board_write_translate = (ImageView) view.findViewById(R.id.board_write_translate);
-            //번역 후 텍스트
-            this.board_target_translate = (TextView) view.findViewById(R.id.board_target_translate);
+
+            //번역
+            this.board_write_sound = (ImageView) view.findViewById(R.id.board_write_sound);
 
 
             //더보기란
@@ -194,6 +213,26 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
         });
 
 
+        // TTS
+        viewholder.board_write_sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(context, String.valueOf(position), Toast.LENGTH_LONG).show();
+                String mText;
+                mText = viewholder.board_list_text.getText().toString();
+                mTextString = new String[]{mText};
+
+                //AsyncTask 실행
+                mNaverDetectionTask = new NaverDetectionTask();
+                mNaverDetectionTask.execute(mTextString);
+
+            }
+        });
+
+
+
+
+        // 번역 버튼
         viewholder.board_write_translate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -378,6 +417,67 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
         viewholder.board_list_learn.setText(bList.get(position).getprofile_learn());
         Log.d("보드 학습언어", bList.get(position).getprofile_learn());
 
+        //번역 전 -> 학습 언어
+        sourceHolder=bList.get(position).getlogin_learn();
+        //번역 후 -> 가르칠 언어
+        targetHolder=bList.get(position).getLogin_teach();
+
+
+        //영어 일때
+        if(sourceHolder.equals("English")){
+            sourceLang="en";
+            //speaker="clara";
+
+        }
+
+        //한국어 일때
+        if(sourceHolder.equals("Korean")){
+            sourceLang="ko";
+            //speaker="mijin";
+        }
+
+        //일어 일때
+        if(sourceHolder.equals("Japanese")){
+            sourceLang="ja";
+            //speaker="yuri";
+        }
+
+        //중국어 일때
+        if(sourceHolder.equals("Chinese")){
+            sourceLang="zh-CN";
+            //speaker="meimei";
+        }
+
+
+        //영어 일때
+        if(targetHolder.equals("English")){
+            targetLang="en";
+
+        }
+
+        //한국어 일때
+        if(targetHolder.equals("Korean")){
+            targetLang="ko";
+
+        }
+
+        //일어 일때
+        if(targetHolder.equals("Japanese")){
+            targetLang="ja";
+
+        }
+
+        //중국어 일때
+        if(targetHolder.equals("Chinese")){
+            targetLang="zh-CN";
+
+        }
+
+
+
+
+
+
 
         viewholder.board_list_name.setText(bList.get(position).getwriter());
         Log.d("보드 네임", bList.get(position).getwriter());
@@ -387,9 +487,6 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
 
         viewholder.board_list_text.setText(bList.get(position).getcontent());
         Log.d("보드 내용", bList.get(position).getcontent());
-
-        //일단은 GONE 처리
-        viewholder.board_target_translate.setVisibility(View.GONE);
 
 
 
@@ -698,5 +795,227 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.CustomViewHo
 
 
     }
+
+
+
+    //언어감지 먼저
+    private class NaverDetectionTask extends AsyncTask<String[], Void, String> {
+
+        @Override
+        protected String doInBackground(String[]... strings) {
+            //여기서 서버에 요청
+            //APIExamTTS.main(mTextString);
+
+            APIExamDetectLangs.main(mTextString);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+
+            //언어를 감지한 후 speaker 설정완료했으면 TTS로 넘어가기
+            //AsyncTask 실행
+            mNaverTTSTask = new NaverTTSTask();
+            mNaverTTSTask.execute(mTextString);
+
+            //tv_title.setText(result);
+
+            //방금 받은 파일명의 mp3가 있으면 플레이 시키자. 맞나 여기서 하는거?
+            //아닌가 파일을 만들고 바로 실행되게 해야 하나? AsyncTask 백그라운드 작업중에...?
+
+        }
+    }
+
+
+    private class NaverTTSTask extends AsyncTask<String[], Void, String> {
+
+        @Override
+        protected String doInBackground(String[]... strings) {
+            //여기서 서버에 요청
+            APIExamTTS.main(mTextString);
+
+            //APIExamDetectLangs.main(mTextString);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            speaker="";
+
+            //tv_title.setText(result);
+
+            //방금 받은 파일명의 mp3가 있으면 플레이 시키자. 맞나 여기서 하는거?
+            //아닌가 파일을 만들고 바로 실행되게 해야 하나? AsyncTask 백그라운드 작업중에...?
+
+        }
+    }
+
+
+
+
+    // 텍스트 읽기 API
+    public static class APIExamTTS {
+
+        private static String TAG = "APIExamTTS";
+
+
+
+        public static void main(String[] args) {
+            String clientId = "";//애플리케이션 클라이언트 아이디값";
+            String clientSecret = "";//애플리케이션 클라이언트 시크릿값";
+            try {
+                String text = URLEncoder.encode(args[0], "UTF-8"); // 13자
+                String apiURL = "https://naveropenapi.apigw.ntruss.com/voice/v1/tts";
+                URL url = new URL(apiURL);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+                con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+                // post request
+                String postParams = "speaker="+speaker+"&speed=0&text=" + text;
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());///여기서 에러 난다?
+                Log.d(TAG, String.valueOf(wr));
+                wr.writeBytes(postParams);
+                wr.flush();
+                wr.close();
+                int responseCode = con.getResponseCode();
+                BufferedReader br;
+                if(responseCode==200) { // 정상 호출
+                    InputStream is = con.getInputStream();
+                    int read = 0;
+                    byte[] bytes = new byte[1024];
+                    //폴더를 만들어 줘야 겠다. 없으면 새로 생성하도록 해야 한다. 일단 Naver폴더에 저장하도록 하자.
+                    File dir = new File(Environment.getExternalStorageDirectory()+"/", "Naver");
+                    if(!dir.exists()){
+                        dir.mkdirs();
+                    }
+                    // 랜덤한 이름으로 mp3 파일 생성
+                    //String tempname = Long.valueOf(new Date().getTime()).toString();
+                    String tempname = "naverttstemp"; //하나의 파일명으로 덮어쓰기 하자.
+                    File f = new File(Environment.getExternalStorageDirectory() + File.separator + "Naver/" + tempname + ".mp3");
+                    f.createNewFile();
+                    OutputStream outputStream = new FileOutputStream(f);
+                    while ((read =is.read(bytes)) != -1) {
+                        outputStream.write(bytes, 0, read);
+                    }
+                    is.close();
+
+                    //여기서 바로 재생하도록 하자. mp3파일 재생 어떻게 하지? 구글링!
+                    String Path_to_file = Environment.getExternalStorageDirectory()+ File.separator+"Naver/"+tempname+".mp3";
+                    MediaPlayer audioPlay = new MediaPlayer();
+                    audioPlay.setDataSource(Path_to_file);
+                    audioPlay.prepare();//이걸 해줘야 하는군. 없으면 에러난다.
+                    audioPlay.start();
+                    //재생하고 나서 파일을 지워줘야 하나? 이거참 고민이네... if문으로 분기 시켜야 하나?
+                    //아니면 유니크한 파일로 만들지 말고 하나의 파일명으로 저장하게 할수도 있을듯...
+
+
+
+                } else {  // 에러 발생
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = br.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    br.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+
+    // 언어감지
+    public static class APIExamDetectLangs {
+
+        private static String TAG = "APIExamDetectLangs";
+
+        public static void main(String[] args) {
+            String clientId = "";//애플리케이션 클라이언트 아이디값";
+            String clientSecret = "";//애플리케이션 클라이언트 시크릿값";
+            try {
+                String query = URLEncoder.encode(args[0], "UTF-8");
+                String apiURL = "https://openapi.naver.com/v1/papago/detectLangs";
+                URL url = new URL(apiURL);
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("X-Naver-Client-Id", clientId);
+                con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+                // post request
+                String postParams = "query=" + query;
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                Log.d(TAG, String.valueOf(wr));
+                wr.writeBytes(postParams);
+                wr.flush();
+                wr.close();
+                int responseCode = con.getResponseCode();
+                BufferedReader br;
+                if(responseCode==200) { // 정상 호출
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                } else {  // 에러 발생
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                }
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+
+                //Json 파싱해서 언어만 가져온 후 speaker 대입하기
+                try
+                {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    String language = jsonObject.getString("langCode");
+
+
+                    if(language.equals("ko")){
+                        //tv_title.setText("Korean");
+                        speaker="mijin";
+                    }
+                    if(language.equals("en")){
+                        //tv_title.setText("English");
+                        speaker="clara";
+                    }
+                    if(language.equals("ja")){
+                        //tv_title.setText("Japan");
+                        speaker="yuri";
+                    }
+                    if(language.equals("zh-CN")){
+                        //tv_title.setText("Chinese");
+                        speaker="meimei";
+                    }
+
+
+
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+
+
+
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+
+
 
 }
